6667feb3072ff4ddd8edaa9d0043fda1bde14935
Donis03ch <111046453+Donis03ch@users.noreply.github.com>
2024-05-24 16:08:56 +0200

    Update script.js

--- movies-website/js/script.js
+++ movies-website/js/script.js
@@ -1,1 +1,13 @@
-// JS
+$(".menu .icon").click(function () {
+    $(".icon").removeClass("active");
+    $(this).addClass("active");
+
+    $(".contents").css("display" , "done");
+    var contentIndex = $(".icon").index(this);
+    $(".content .contents").eq(contentIndex).css("display", "grid" );
+});
+
+$(".category").click(function () {
+    $(".category").removeClass("active");
+    $(this).addClass("active");
+})
